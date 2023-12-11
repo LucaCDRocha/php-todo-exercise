27967bb13429701d922cf61b7f40d2b38ba54935
LucaCDRocha <lucadrocha@gmail.com>
2023-12-11 09:43:16 +0100

    Change title

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ $items = $db->query($selectQuery);
     <div class="navbar navbar-dark bg-dark shadow-sm">
       <div class="container d-flex justify-content-between">
         <a href="#" class="navbar-brand d-flex align-items-center">
-          <strong>TodoList</strong>
+          <strong>Lise des tâches</strong>
         </a>
       </div>
     </div>
